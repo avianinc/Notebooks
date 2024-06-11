968c69d4ffcd22646dec71d00919d3796b1bc76c
John K. DeHart <jdehart@avian.com>
2024-06-10 20:02:34 -0400

    added ontolgy and scripts

---
+++ scripts/test.py
@@ -0,0 +1,15 @@
+import sys
+
+def multiply(a, b):
+    return a * b
+
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        print("Usage: python test.py <number1> <number2>")
+        exit(1)
+
+    number1 = float(sys.argv[1])
+    number2 = float(sys.argv[2])
+
+    result = multiply(number1, number2)
+    print(result)
