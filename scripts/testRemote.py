d0611d613a8b14229406439d3821dc2377d897a4
avianllc <93828723+avianinc@users.noreply.github.com>
2024-06-17 15:18:12 -0400

    Update testRemote.py
    reverted

--- scripts/testRemote.py
+++ scripts/testRemote.py
@@ -8,19 +8,8 @@ if __name__ == "__main__":
         print("Usage: python test.py <number1> <number2>")
         exit(1)
 
-    number1 = sys.argv[1]
-    number2 = sys.argv[2]
-
-    # Report the types of input variables
-    print(f"The type of number1 is: {type(number1)}")
-    print(f"The type of number2 is: {type(number2)}")
-
-    try:
-        number1 = float(number1)
-        number2 = float(number2)
-    except ValueError:
-        print("Both inputs should be numbers.")
-        exit(1)
+    number1 = float(sys.argv[1])
+    number2 = float(sys.argv[2])
 
     result = multiply(number1, number2)
-    print(f"The multiplication result is: {result}")
+    print(result)
